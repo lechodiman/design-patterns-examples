261ea281fcaf76a25d1ad0095c024771da3c6676
Luis Chodiman <lechodiman@uc.cl>
2019-10-11 15:24:07 -0300

    :sparkles: factory method

---
+++ factory-method/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace factory_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface ImageReader
+    {
+        DecodedImage GetDecodedImage();
+    }
+
+    class DecodedImage
+    {
+        private string _image;
+
+        public DecodedImage(string image)
+        {
+            _image = image;
+        }
+
+        public override string ToString()
+        {
+            return _image + ": is decoded";
+        }
+    }
+
+    class GifReader : ImageReader
+    {
+        private DecodedImage _decodedImage;
+
+        public GifReader(string image)
+        {
+            _decodedImage = new DecodedImage(image);
+        }
+
+        public DecodedImage GetDecodedImage()
+        {
+            return _decodedImage;
+        }
+    }
+}
